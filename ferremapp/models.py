710605f942430facd869800896af853141ccb78c
Fabián Espinoza M. <faboespinozam@icloud.com>
2024-04-25 01:09:07 -0400

    :v

--- ferremapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class Usuario(AbstractUser):
-    is_admin = models.BooleanField('Es Admin', default=False)
-    is_vendedor = models.BooleanField('Es Vendedor', default=False)
-    is_bodeguero = models.BooleanField('Es Bodeguero', default=False)
-    is_contador = models.BooleanField('Es Contador', default=False)
-    is_cliente = models.BooleanField('Es Cliente', default=False)
